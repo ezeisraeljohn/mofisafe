bb3b3e360bbe9f9e49c4638f20d58f4799379b21
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-17 18:03:10 +0100

    chore: Add setup_mysql.sql script to prepare MySQL server for the project

---
+++ setup_mysql.sql
@@ -0,0 +1,10 @@
+-- This prepares a mysql server for the project
+-- creates a new database for this project
+
+CREATE DATABASE IF NOT EXISTS mofisafe_dev_db;
+
+CREATE USER IF NOT EXISTS 'mofisafe_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'mofisaf_dev'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'mofisafe_dev'@'localhost';
