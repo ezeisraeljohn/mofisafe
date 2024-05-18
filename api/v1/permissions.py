be57f8fa423443b03f901917f59aa2c294c353c1
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-18 03:00:54 +0100

    refactor: Add IsOwnerOrReadOnly permission class for income API endpoints

---
+++ api/v1/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    "This will give access to authorize owners to make changes to an object"
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.owner == request.user
