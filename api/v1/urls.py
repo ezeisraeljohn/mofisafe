7b98331b90eb7c7facdb7371b56e54ec340b99e0
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-18 02:32:17 +0100

    refactor: Add UserList and UserDetail views for user API endpoints

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from api.v1.views import income
+from api.v1.views import user
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
         path('api/v1/income/', income.IncomeList.as_view(), name='income_list'),
-        path('api/v1/income/<int:pk>/', income.IncomeDetails.as_view(), name='income_details')
+        path('api/v1/income/<int:pk>/', income.IncomeDetail.as_view(), name='income_details'),
+        path('api/v1/users/', user.UserList.as_view(), name="users_list"),
+        path('api/v1/user/<int:pk>/', user.UserDetail.as_view(), name="user_details"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
