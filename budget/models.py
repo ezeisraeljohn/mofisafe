352d5660fc50ee992174aa8f28115e1854e498aa
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-20 09:07:23 +0100

    refactor: Add initial migration for Budget model

--- budget/models.py
+++ budget/models.py
@@ -7,9 +7,6 @@ from django.contrib.auth.models import User
 class Budget(BaseModel):
     """ This class represents the budget model.
 
-    Methods:
-        __str__(): Return a human readable representation of the model instance.
-
     Example:
         budget = Budget(amount=1000, category='Rent', user_id=1)
         budget.save()
@@ -19,8 +16,3 @@ class Budget(BaseModel):
     period = models.CharField(max_length=100, default='monthly')
     start_date = models.DateTimeField(default=timezone.now)
     end_date = models.DateTimeField(default=None)
-
-
-    def __str__(self):
-        """ Return a human readable representation of the model instance. """
-        return str({f'{type(self).__name__}.{self.id}: {self.__dict__}'})
