b2f23ccdcaadc7e1828d4a4b376b5d1424650590
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-20 09:01:00 +0100

    refactor: Remove unused code from admin.py

--- mofisafe_app/admin.py
+++ mofisafe_app/admin.py
@@ -1,6 +1,2 @@
 from django.contrib import admin
-from mofisafe_app.models import Expense, Income, Budget
-# Register your models here.
-
-models = [Expense, Income, Budget]
-admin.site.register(models)
+# # Register your models here.
