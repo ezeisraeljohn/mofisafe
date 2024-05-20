c26ddd0a67b50fb81db48d9dda1c1d41affec105
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-20 08:58:07 +0100

    refactor: Update income.py to use viewsets and include IsOwnerOrReadOnly permission class

--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -1,19 +1,11 @@
-from rest_framework import generics
 from rest_framework.decorators import permission_classes
+from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from api.v1.serializers import SerializeUser
 from django.contrib.auth.models import User
 
 
-@permission_classes([AllowAny])
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     """ This gets all users """
     queryset = User.objects.all()
     serializer_class = SerializeUser
-
-@permission_classes([AllowAny])
-class UserDetail(generics.RetrieveAPIView):
-    """ This retrieves a user """
-    queryset = User.objects.all()
-    serializer_class = SerializeUser
-
