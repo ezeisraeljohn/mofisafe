9a25b258b153b56bd8063b4ec200e1a4d53697d6
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-20 19:14:28 +0100

    refactor: Update import statement for SerializeExpense in expense.py

--- api/v1/views/expense.py
+++ api/v1/views/expense.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from expenses.models import Expense
 from rest_framework import permissions
-from api.v1.serializers import SerializeExpense
+from expenses.serializer import SerializeExpense
 from api.v1.permissions import IsOwnerOrReadOnly
 
 
