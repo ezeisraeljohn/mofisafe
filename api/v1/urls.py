134ce324f500b4814ff01921f81f42cca612372f
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-05-21 15:49:52 +0100

    Refactor project structure for modularity by splitting functionality into separate apps

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,52 +1,53 @@
 from django.urls import path
-from api.v1.views import income
-from api.v1.views import user
-from api.v1.views import expense
-from api.v1.views import budget
+from income import views as incomeViews
+from users import views as usersViews
+from expenses import views as expenseViews
+from budget import views as budgetViews
 from rest_framework.urlpatterns import format_suffix_patterns
 from api.v1.views import api_root
 
 
-income_list = income.IncomeViewSet.as_view({
+income_list = incomeViews.IncomeViewSet.as_view({
         'get': 'list',
         'post': 'create'
 })
 
-income_details = income.IncomeViewSet.as_view({
+income_details = incomeViews.IncomeViewSet.as_view({
         'get': 'retrieve',
         'put': 'update',
         'delete': 'destroy'
 })
 
-expense_list = expense.ExpenseViewSet.as_view({
+expense_list = expenseViews.ExpenseViewSet.as_view({
         'get': 'list',
         'post': 'create'
 })
 
-expense_details = expense.ExpenseViewSet.as_view({
+expense_details = expenseViews.ExpenseViewSet.as_view({
         'get':'list',
         'put':'update',
         'delete': 'destroy'
 })
 
-budget_list = budget.BudgetViewSet.as_view({
+budget_list = budgetViews.BudgetViewSet.as_view({
         'get':'list',
         'post': 'create'
 })
 
-budget_details = budget.BudgetViewSet.as_view({
+budget_details = budgetViews.BudgetViewSet.as_view({
         'get': 'retrieve',
         'put': 'update',
         'delete': 'destroy'
 })
 
-user_list = user.UserViewSet.as_view({
+user_list = usersViews.UserViewSet.as_view({
         'get': 'list'
 })
 
-user_details = user.UserViewSet.as_view({
+user_details = usersViews.UserViewSet.as_view({
         'get': 'retrieve'
 })
+
 
 urlpatterns = [
         path('income/', income_list, name='income_list'),
--- income/views.py
+++ income/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework import viewsets
+from .models import Income
+from .serializer import SerializeIncome
+from utils.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class IncomeViewSet(viewsets.ModelViewSet):
+
+    queryset = Income.objects.all()
+    serializer_class = SerializeIncome
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
