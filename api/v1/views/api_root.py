66d299b665a9a8bafda8992337e51801fc33ec66
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-21 08:18:01 +0100

    feat: Add expenses and budgets to API root response

--- api/v1/views/api_root.py
+++ api/v1/views/api_root.py
@@ -8,5 +8,7 @@ def api_root(request, format=None):
     """ Get the root api"""
     return Response({
         'users': reverse('users_list', request=request, format=format),
-        'incomes': reverse('income_list', request=request, format=format)
+        'incomes': reverse('income_list', request=request, format=format),
+        'expenses': reverse('expense_list', request=request, format=format),
+        'budgets': reverse('budget_list', request=request, format=format)
     })
