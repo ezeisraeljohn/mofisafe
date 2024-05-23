2154ffbced675206959f994a9052668c525ce28d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-23 16:04:11 +0100

    chore: Add mofisafe_app to urlpatterns in urls.py

--- mofisafe/urls.py
+++ mofisafe/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
         path('admin/', admin.site.urls),
         path('api/v1/', include('api.v1.urls')),
         path('api/v1/auth/', include('rest_framework.urls')),
+        path('', include('mofisafe_app.urls')),
 ]
