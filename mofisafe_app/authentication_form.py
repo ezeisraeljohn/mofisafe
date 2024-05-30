34a1e1e1df82bc3bc6a2f6c3459fc2608eef3b7c
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-05-30 10:36:29 +0100

    feat: Add custom authentication form with username field styling

---
+++ mofisafe_app/authentication_form.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600'
+        })
+    )
+    lastname = forms.CharField(max_length=10)
