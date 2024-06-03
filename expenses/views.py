8f6a0d57aa8c23b1db085237ce3821b571ea3228
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-03 10:32:37 +0100

    chore: Deduct expense amount from category balance

--- expenses/views.py
+++ expenses/views.py
@@ -14,9 +14,27 @@ class ExpenseViewSet(viewsets.ModelViewSet):
     serializer_class = SerializeExpense
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Expense.objects.filter(user=self.request.user)
+        return Expense.objects.none()
+    
     def perform_create(self, serializer):
         if 'date' not in serializer.validated_data:
             serializer.save(user=self.request.user, date=timezone.now())
+
+        amount = serializer.validated_data.get('amount')
+        category = serializer.validated_data.get('category')
+        user = self.request.user
+        
+        if category.user != user:
+            return Response({"Error":"This category does not belong to you"})
+
+        category.category_balance -= amount
+
+        category.save()
+
         serializer.save(user=self.request.user)
 
     def perform_update(self, serializer):
