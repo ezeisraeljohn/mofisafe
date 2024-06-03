fc6fbb5b3708619e31c678e6049058bae246a16e
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-03 10:30:13 +0100

    chore: Update SerializeCategories to include category_balance field

--- categories/serializer.py
+++ categories/serializer.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from categories.models import Categories
 
-class SerializeCategories(serializers.ModelSerializer):
+class SerializeCategories(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Categories
-        fields = ['id', 'url', 'name', 'description', 'user']
+        fields = ['id', 'type', 'url', 'name', 'description', 'user', 'category_balance']
         read_only_fields = ['user']
 
     def validate_name(self, value):
