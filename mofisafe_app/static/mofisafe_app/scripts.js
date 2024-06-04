55fca73264426b5206bd5825d2ecc6d928116fb7
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-04 21:01:56 +0100

    feat: Update Expenses model to limit category choices to income category type

--- mofisafe_app/static/mofisafe_app/scripts.js
+++ mofisafe_app/static/mofisafe_app/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
-  // Get the total income available
+  let total_weekly_income = 0;
+  let total_weekly_expense = [];
+  const list = new Array(7).fill(0);
+  const list_expense = new Array(7).fill(0);
 
+  // This function gets the week of the month
+  function getWeekOfMonth(date) {
+    const firstDayOfMonth = moment(date).startOf("month");
+    const weekOfYear = moment(date).week();
+    const firstWeekOfYear = firstDayOfMonth.week();
+
+    return weekOfYear - firstWeekOfYear + 1;
+  }
+  // Gets Available Balance
   $.ajax({
     url: "http://127.0.0.1:8000/api/v1/categories/",
     method: "GET",
@@ -17,6 +29,7 @@ $(document).ready(function () {
     },
   });
 
+  // Gets the total expense for a month
   $.ajax({
     url: "http://127.0.0.1:8000/api/v1/expenses/",
     method: "GET",
@@ -36,6 +49,7 @@ $(document).ready(function () {
     },
   });
 
+  // Get the total income for a week a create a chart for it
   $.ajax({
     url: "http://127.0.0.1:8000/api/v1/income/",
     method: "GET",
@@ -55,15 +69,6 @@ $(document).ready(function () {
     },
   });
 
-  // This function gets the week of the month
-  function getWeekOfMonth(date) {
-    const firstDayOfMonth = moment(date).startOf("month");
-    const weekOfYear = moment(date).week();
-    const firstWeekOfYear = firstDayOfMonth.week();
-
-    return weekOfYear - firstWeekOfYear + 1;
-  }
-
   $.ajax({
     url: "http://127.0.0.1:8000/api/v1/income/",
     method: "GET",
@@ -71,9 +76,7 @@ $(document).ready(function () {
       Authorization: "Basic " + btoa("ezeisraeljohn:testuser123"),
     },
     success: function (data) {
-      const list = new Array(7).fill(0);
       const currentDate = moment().startOf("day"); // Get the current date and reset the time to midnight
-      let total_weekly_income = 0;
 
       data.forEach(function (income) {
         const income_date = moment(income.date, "YYYY-MM-DD").startOf("day"); // Reset the time to midnight for the income date
@@ -88,7 +91,6 @@ $(document).ready(function () {
       });
 
       // Create a chart for the weekly income
-      console.log(list);
       const areaChartOptions = {
         chart: {
           height: "350",
@@ -174,215 +176,234 @@ $(document).ready(function () {
     },
   });
 
-  const getChartOptions = () => {
-    return {
-      series: [35.1, 23.5, 2.4, 5.4],
-      colors: ["#1C64F2", "#16BDCA", "#FDBA8C", "#E74694"],
-      chart: {
-        height: 350,
-        width: "100%",
-        type: "donut",
-      },
-      stroke: {
-        colors: ["transparent"],
-        lineCap: "",
-      },
-      plotOptions: {
-        pie: {
-          donut: {
-            labels: {
-              show: true,
-              name: {
-                show: true,
-                fontFamily: "Inter, sans-serif",
-                offsetY: 20,
-              },
-              total: {
-                showAlways: true,
-                show: true,
-                label: "Unique visitors",
-                fontFamily: "Inter, sans-serif",
-                formatter: function (w) {
-                  const sum = w.globals.seriesTotals.reduce((a, b) => {
-                    return a + b;
-                  }, 0);
-                  return "$" + sum + "k";
+  // This gets categories and create a chart for it
+  $.ajax({
+    url: "http://127.0.0.1:8000/api/v1/categories/",
+    method: "GET",
+    headers: {
+      Authorization: "Basic " + btoa("ezeisraeljohn:testuser123"),
+    },
+    success: function (data) {
+      const dict = {};
+      data.forEach(function (cat) {
+        if (cat.type === "income") {
+          if (!dict[cat.name]) {
+            dict[cat.name] = parseFloat(cat.category_balance);
+          } else {
+            dict[cat.name] += parseFloat(cat.category_balance);
+          }
+        }
+      });
+      console.log(dict);
+      const series = [];
+      const labels = [];
+      for (const [label, serie] of Object.entries(dict)) {
+        series.push(serie);
+        labels.push(label);
+      }
+      const getChartOptions = () => {
+        return {
+          series: series,
+          colors: ["#1C64F2", "#16BDCA", "#FDBA8C", "#E74694"],
+          chart: {
+            height: 350,
+            width: "100%",
+            type: "donut",
+          },
+          stroke: {
+            colors: ["transparent"],
+            lineCap: "",
+          },
+          plotOptions: {
+            pie: {
+              donut: {
+                labels: {
+                  show: true,
+                  name: {
+                    show: true,
+                    fontFamily: "Inter, sans-serif",
+                    offsetY: 20,
+                  },
+                  total: {
+                    showAlways: true,
+                    show: true,
+                    label: "In Income",
+                    fontFamily: "Inter, sans-serif",
+                    formatter: function (w) {
+                      const sum = w.globals.seriesTotals.reduce((a, b) => {
+                        return a + b;
+                      }, 0);
+                      return "$" + sum;
+                    },
+                  },
+                  value: {
+                    show: true,
+                    fontFamily: "Inter, sans-serif",
+                    offsetY: -20,
+                    formatter: function (value) {
+                      return "$" + value;
+                    },
+                  },
                 },
-              },
-              value: {
-                show: true,
-                fontFamily: "Inter, sans-serif",
-                offsetY: -20,
-                formatter: function (value) {
-                  return value + "k";
-                },
+                size: "80%",
               },
             },
-            size: "80%",
           },
-        },
-      },
-      grid: {
-        padding: {
-          top: -2,
-        },
-      },
-      labels: ["Direct", "Sponsor", "Affiliate", "Email marketing"],
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        position: "bottom",
-        fontFamily: "Inter, sans-serif",
-      },
-      yaxis: {
-        labels: {
-          formatter: function (value) {
-            return value + "k";
+          grid: {
+            padding: {
+              top: -2,
+            },
           },
-        },
-      },
-      xaxis: {
-        labels: {
-          formatter: function (value) {
-            return value + "k";
+          labels: labels,
+          dataLabels: {
+            enabled: false,
           },
-        },
-        axisTicks: {
-          show: false,
-        },
-        axisBorder: {
-          show: false,
-        },
-      },
-    };
-  };
+          legend: {
+            position: "bottom",
+            fontFamily: "Inter, sans-serif",
+          },
+          yaxis: {
+            labels: {
+              formatter: function (value) {
+                return "$" + value;
+              },
+            },
+          },
+          xaxis: {
+            labels: {
+              formatter: function (value) {
+                return value + "k";
+              },
+            },
+            axisTicks: {
+              show: false,
+            },
+            axisBorder: {
+              show: false,
+            },
+          },
+        };
+      };
 
-  if ($("#donut-chart").length && typeof ApexCharts !== "undefined") {
-    const chart = new ApexCharts(
-      document.getElementById("donut-chart"),
-      getChartOptions()
-    );
-    chart.render();
-
-    // Get all the checkboxes by their class name
-    const checkboxes = $('#devices input[type="checkbox"]');
-
-    // Function to handle the checkbox change event
-    function handleCheckboxChange(event) {
-      const checkbox = event.target;
-      if (checkbox.checked) {
-        switch (checkbox.value) {
-          case "desktop":
-            chart.updateSeries([15.1, 22.5, 4.4, 8.4]);
-            break;
-          case "tablet":
-            chart.updateSeries([25.1, 26.5, 1.4, 3.4]);
-            break;
-          case "mobile":
-            chart.updateSeries([45.1, 27.5, 8.4, 2.4]);
-            break;
-          default:
-            chart.updateSeries([55.1, 28.5, 1.4, 5.4]);
-        }
-      } else {
-        chart.updateSeries([35.1, 23.5, 2.4, 5.4]);
+      if ($("#donut-chart").length && typeof ApexCharts !== "undefined") {
+        const chart = new ApexCharts(
+          document.getElementById("donut-chart"),
+          getChartOptions()
+        );
+        chart.render();
       }
-    }
+    },
+  });
 
-    // Attach the event listener to each checkbox
-    checkboxes.each(function () {
-      $(this).on("change", handleCheckboxChange);
-    });
-  }
+  $.ajax({
+    url: "http://127.0.0.1:8000/api/v1/expenses/",
+    method: "GET",
+    headers: {
+      Authorization: "Basic " + btoa("ezeisraeljohn:testuser123"),
+    },
+    success: function (data) {
+      const currentDate = moment().startOf("day"); // Get the current date and reset the time to midnight
 
-  const chartOptions = {
-    yaxis: {
-      show: false,
-      labels: {
-        formatter: function (value) {
-          return "€" + value;
+      data.forEach(function (expense) {
+        const expense_date = moment(expense.date, "YYYY-MM-DD").startOf("day"); // Reset the time to midnight for the income date
+
+        // Check if the income date is in the same week of the month as the current date
+        if (getWeekOfMonth(expense_date) === getWeekOfMonth(currentDate)) {
+          const dayOfWeek = expense_date.day();
+          list_expense[dayOfWeek] += parseFloat(expense.amount);
+          total_weekly_expense += parseFloat(expense.amount);
+        }
+        // $(".income-week").text(`$${total_weekly_income}`);
+      });
+      const chartOptions = {
+        yaxis: {
+          show: false,
+          labels: {
+            formatter: function (value) {
+              return "$" + value;
+            },
+          },
         },
-      },
-    },
-    chart: {
-      height: 350,
-      maxWidth: "100%",
-      type: "area",
-      fontFamily: "Inter, sans-serif",
-      dropShadow: {
-        enabled: false,
-      },
-      toolbar: {
-        show: false,
-      },
-    },
-    tooltip: {
-      enabled: true,
-      x: {
-        show: false,
-      },
-    },
-    fill: {
-      type: "gradient",
-      gradient: {
-        opacityFrom: 0.55,
-        opacityTo: 0,
-        shade: "#1C64F2",
-        gradientToColors: ["#1C64F2"],
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      width: 6,
-    },
-    grid: {
-      show: false,
-      strokeDashArray: 4,
-      padding: {
-        left: 2,
-        right: 2,
-        top: -26,
-      },
-    },
-    series: [
-      {
-        name: "Developer Edition",
-        data: [1500, 1418, 1456, 1526, 1356, 1256],
-        color: "#1A56DB",
-      },
-      {
-        name: "Designer Edition",
-        data: [643, 413, 765, 412, 1423, 1731],
-        color: "#7E3BF2",
-      },
-    ],
-    xaxis: {
-      categories: [
-        "01 Feb",
-        "02 Feb",
-        "03 Feb",
-        "04 Feb",
-        "05 Feb",
-        "06 Feb",
-        "07 Feb",
-      ],
-      labels: {
-        show: false,
-      },
-      axisBorder: {
-        show: false,
-      },
-      axisTicks: {
-        show: false,
-      },
-    },
-  };
+        chart: {
+          height: 350,
+          maxWidth: "100%",
+          type: "area",
+          fontFamily: "Inter, sans-serif",
+          dropShadow: {
+            enabled: false,
+          },
+          toolbar: {
+            show: false,
+          },
+        },
+        tooltip: {
+          enabled: true,
+          x: {
+            show: false,
+          },
+        },
+        fill: {
+          type: "gradient",
+          gradient: {
+            opacityFrom: 0.55,
+            opacityTo: 0,
+            shade: "#1C64F2",
+            gradientToColors: ["#1C64F2"],
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          width: 6,
+        },
+        grid: {
+          show: false,
+          strokeDashArray: 4,
+          padding: {
+            left: 2,
+            right: 2,
+            top: -26,
+          },
+        },
+        series: [
+          {
+            name: "Income",
+            data: list,
+            color: "#1A56DB",
+          },
+          {
+            name: "Expenses",
+            data: list_expense,
+            color: "#7E3BF2",
+          },
+        ],
+        xaxis: {
+          categories: [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+          ],
+          labels: {
+            show: false,
+          },
+          axisBorder: {
+            show: false,
+          },
+          axisTicks: {
+            show: false,
+          },
+        },
+      };
 
-  if ($("#main-chart").length && typeof ApexCharts !== "undefined") {
-    const chart = new ApexCharts($("#main-chart")[0], chartOptions);
-    chart.render();
-  }
+      if ($("#main-chart").length && typeof ApexCharts !== "undefined") {
+        const chart = new ApexCharts($("#main-chart")[0], chartOptions);
+        chart.render();
+      }
+    },
+  });
 });
