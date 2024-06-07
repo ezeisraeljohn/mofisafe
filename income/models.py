1dae13710ab8d899c0cfc0d14589f0bb3d6fd5c7
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-07 10:44:57 +0100

    Refactor Income model to set default value for date field

--- income/models.py
+++ income/models.py
@@ -5,11 +5,7 @@ from core.models import BaseModel
 from categories.models import Categories
 
 class Income(BaseModel):
-    """This class represents the income model.
-
-    Methods:
-        __str__(): Return a human readable representation of the model instance.
-
+    """This class represents the income model
     Example:
         income = Income(amount=1000, description='Salary',
         date=datetime.now(), source='Salary', category='Salary', user_id=1)
@@ -17,6 +13,6 @@ class Income(BaseModel):
     """
     user = models.ForeignKey(User, related_name='income', on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=15, decimal_places=2)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=timezone.now)
     source = models.CharField(max_length=100)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name='categories', limit_choices_to={'category_type': 'income'})
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, null=True, related_name='categories', limit_choices_to={'category_type': 'income'})
